090e15878fdeace95c9d21c6d615d60226a28f13
wesleykang13 <wesleykang123@gmail.com>
2026-02-05 10:15:11 +0000

    updated providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\RateLimiter; // 🟢 Import this
-use Illuminate\Cache\RateLimiting\Limit;    // 🟢 Import this
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
